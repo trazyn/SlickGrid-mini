508e8046678189db2249325a15fe73d2aa1f5a1b
Trazyn <Trazyn@users.noreply.github.com>
2014-09-25 20:34:33 +0800

    Update and rename Filter.js to Conditions.js

---
+++ src/paging/Conditions.js
@@ -0,0 +1,96 @@
+
+define( function() {
+
+	var handleHeaderRowCellRendered = function( e, args ) {
+	
+		args.column.filter && $( args.node )
+			.html( "<input type='text' data-column-field='" + args.column.field + "' >" );
+	}
+	
+	, getSort = function() {
+		
+		var sortColumn = this.getSortColumns()[ 0 ], column;
+
+		if ( !sortColumn ) {
+		
+			return {};
+		}
+		
+		column = $G.getColumns()[ $G.getColumnIndex( sortColumn.columnId ) ];
+
+		return {
+			pageVO: {
+			
+				remoteSortField: column.field,
+				remoteSortOrder: sortColumn.sortAsc ? "asc" : "desc"
+			}
+		};
+	};
+
+	return function( $G, isAjax ) {
+	
+		var dataView = $G.getData(), filters = {};
+	
+		$G.onHeaderRowCellRendered.unsubscribe( handleHeaderRowCellRendered );
+		$G.onHeaderRowCellRendered.subscribe( handleHeaderRowCellRendered );
+
+		/** Set the filter */
+		dataView.setFilter( function( row ) {
+			
+			var value;
+
+			for ( var field in filters ) {
+
+				value = filters[ field ];
+
+				if ( value && row[ field ].toString().indexOf( value ) === -1 ) {
+					return false;
+				}
+			}
+			return true;
+		} );
+
+		$( $G.getHeaderRow() )
+
+		/** Remove all keyup handler */
+		.undelegate( "input:visible", "keyup" )	
+
+		.delegate( "input:visible", "keyup", function( e ) {
+			
+			var self = $( this )
+			, field = self.attr( "data-column-field" );
+
+			e.stopPropagation();
+
+			if ( field ) {
+				
+				filters[ field ] = self.val().replace( /^\s+|\s+$/g, "" );
+				dataView.refresh();
+
+				if ( isAjax && 13 === e.keyCode ) {
+					
+					dataView.onPagingInfoChanged.notify( { doSearch: 1 } );
+				}
+			}
+		} );
+
+		return {
+
+			getVO: function( args ) {
+				var criteria = [];
+
+				for ( var field in filters ) {
+					
+					criteria.push( {
+						
+						field: field,
+						operator: "like",
+						value: filters[ field ]
+					} );
+				}
+
+				return $.extend( true, {}, getSort.call( $G ), { criteria: JSON.stringify( criteria ) }, args || {} );
+			}
+		};
+	};
+} );
