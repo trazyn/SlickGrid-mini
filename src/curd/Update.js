21d82c95a0a80f5cb38c10de53eb037c892dc19d
Trazyn <Trazyn@users.noreply.github.com>
2014-10-05 19:06:38 +0800

    Update Update.js

--- src/curd/Update.js
+++ src/curd/Update.js
@@ -20,30 +20,42 @@ define( function() {
 				handler
 					.subscribe( $G.onBeforeEditCell, function( e, args ) {
 						
-						var node = $( $G.getCellNode( args.row, args.cell ) );
+						var node = $( $G.getCellNode( args.row, args.cell ) )
 
-						node.is( "[data-original]" )
-							|| node.attr( "data-original", args.item[ args.column.field ] );
+						, item = args.item;
+
+						item[ "_" ] = item[ "_" ] || {};
+
+						if ( !item[ "_" ].hasOwnProperty( args.column.field ) ) {
+							item[ "_" ][ args.column.field ] = item[ args.column.field ];
+						}
 					} )
 		
 					.subscribe( $G.onCellChange, function( e, args ) {
 						
-						var index = args.row, hash = $G.getCellCssStyles( settings.key ) || {};
+						var index = args.row
+						, hash = $G.getCellCssStyles( settings.key ) || {}
+						, item = args.item;
 
 						hash[ index ] = hash[ index ] || {};
 
-						if ( $( $G.getCellNode( args.row, args.cell ) )
-							.attr( "data-original" ) === args.item[ args.column.field ] ) {
+						if ( item[ "_" ][ args.column.field ] === item[ args.column.field ] ) {
 
+							delete hash[ index ][ args.column.field ];
+						
 							if ( 0 === --hash[ index ][ "_length_" ] ) {
 								
 								delete hash[ index ];
+								delete item[ "grid_action" ];
 								$G.invalidateRow( index );
 							}
-						} else {
+						}
+						else {
 
 							hash[ index ][ args.column.id ] = settings.cssClass;
 							hash[ index ][ "_length_" ] = (hash[ index ][ "_length_" ] || 0) + 1;
+
+							item[ "_isNew" ] || (item[ "grid_action" ] = "update");
 						}
 
 						$G.setCellCssStyles( settings.key, hash );
@@ -69,7 +81,9 @@ define( function() {
 
 				for ( var idx in this.getUpdateRowsHash() ) {
 
-					!deletes[ idx ] && !adds[ idx ] && rows.push( dataView.getItemByIdx( idx ) );
+					if ( !deletes[ idx ] && !adds[ idx ] ) {
+						rows.push( $G.getDataItem( idx ) );
+					}
 				}
 
 				return rows;
