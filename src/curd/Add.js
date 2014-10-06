9b164d6dc5c79d9071c9cca6cbb35ad977be61d4
Trazyn <Trazyn@users.noreply.github.com>
2014-10-06 19:28:23 +0800

    Update Add.js

--- src/curd/Add.js
+++ src/curd/Add.js
@@ -1,19 +1,12 @@
 
 define( function() {
 
+	"use strict";
+
 	var defaults = { 
 		
-		add: {
-		
-			key: "add",
-			cssClass: "slick-add"
-		},
-
-		invalid: {
-			
-			key: "invalid",
-			cssClass: "slick-invalid"
-		}
+		key: "add",
+		cssClass: "slick-add"
 	}
 
 	, Add = function( $G, settings ) {
@@ -21,39 +14,40 @@ define( function() {
 		var adds = {}, index = 0
 
 		, dataView = $G.getData()
-		, handler = new Slick.EventHandler(), inHandler;
+		, handler = new Slick.EventHandler();
 
 		$.extend( this, {
 
 			init: function() {
 			
-				handler
-					.subscribe( $G.onValidationError, function( e, args ) {
-						
-						var hash = $G.getCellCssStyles( settings.invalid.key ) || {};
-
-						hash[ args.row ] = hash[ args.row ] || {};
-
-						hash[ args.row ][ args.column.id ] = settings.invalid.cssClass;
-
-						if ( !inHandler ) {
-
-							$G.setCellCssStyles( settings.invalid.key, hash );
-						}
-
-					} )
+				var inHandler = false
+				, syncStyle = function( e, args ) {
 				
-					.subscribe( $G.onCellChange, function( e, args ) {
-					
-						var hash = $G.getCellCssStyles( settings.invalid.key ) || {};
+					var hash = {}, idxById;
 
-						if ( hash[ args.row ] ) {
-							
-							delete hash[ args.row ];
-							$G.invalidateRow( args.row );
-							$G.setCellCssStyles( settings.invalid.key, hash );
-						}
-					} );
+					if ( inHandler ) { return; }
+
+					idxById = dataView.getSnapshot();
+					inHandler = !inHandler;
+
+					for ( var id in adds ) {
+					
+						hash[ idxById[ id ] ] = adds[ id ];
+					}
+
+					$G.setCellCssStyles( settings.key, hash );
+
+					inHandler = !inHandler;
+				};
+
+				handler
+					.subscribe( $G.onCellCssStylesChanged, function( e, args ) {
+						
+						args.key === settings.key && syncStyle();
+					} )
+
+					.subscribe( dataView.onRowsChanged, syncStyle )
+					.subscribe( dataView.onRowCountChanged, syncStyle );
 			},
 
 			destroy: function() {
@@ -68,16 +62,11 @@ define( function() {
 				
 				var rows = [];
 
-				for ( var idx in $G.getAddRowsHash() ) {
-					rows.push( $G.getDataItem( idx ) );
+				for ( var id in adds ) {
+					rows.push( dataView.getItemById( id ) );
 				}
 
 				return rows;
-			},
-
-			getAddRowsHash: function() {
-				
-				return $G.getCellCssStyles( settings.add.key ) || {};
 			},
 
 			getAddRowsID: function() {
@@ -85,80 +74,75 @@ define( function() {
 				return adds;
 			},
 
-			setAddRows: function( row ) {
+			setAddRows: function( hash ) {
 
-				var columns = $G.getColumns()
-					
-				, hash = this.getAddRowsHash()
+				var columns, id, row;
+
+				if ( hash ) {
+
+					adds = hash;
+				} else {
 				
-				, idx;
-
-				if ( !row ) {
-					
 					row = {
 						
 						rr: "+ " + ++index,
-					    	_isNew: true,
-					    	"grid_action": "add"
+						_isNew: true,
+						"grid_action": "add"
 					};
-				}
+					columns = this.getColumns();
 
-				dataView.beginUpdate();
+					dataView.beginUpdate();
 
-				idx = dataView.getLength();
+					id = row[ "rr" ];
 
-				if ( index === 1 ) {
-				
-					/** Add rows in current viewport */
-					dataView.setPagingOptions( { pageSize: 0 }, false );
-
-					idx = dataView.getItems().length;
-				} 
-
-				for ( var i = columns.length; --i >= 0; ) {
-				
-					var column = columns[ i ];
-
-					if ( column.defaultValue && !row[ column.field ] ) {
-
-						row[ column.field ] = column.defaultValue;
-					}
-
-					hash[ idx ] = hash[ idx ] || {};
-
-					hash[ idx ][ column.id ] = settings.add.cssClass;
-				}
-
-				$G.getData().addItem( row );
-				adds[ row[ "rr" ] ] = row[ "rr" ];
-
-				dataView.endUpdate();
-
-				for ( var i = columns.length; --i >= 0; ) {
+					if ( index === 1 ) {
 					
-					var column = columns[ i ]
+						/** Add rows in current viewport */
+						dataView.setPagingOptions( { pageSize: 0 }, false );
+					} 
 
-					, validator = column.validator;
+					for ( var i = columns.length; --i >= 0; ) {
+					
+						var column = columns[ i ];
 
-					inHandler = i === 0 ? true : false;
+						if ( column.defaultValue && !row[ column.field ] ) {
 
-					if ( "function" === typeof validator ) {
-						
-						var validationResults = validator( row[ column[ "field" ] ], row, column );
+							row[ column.field ] = column.defaultValue;
+						}
 
-						validationResults.valid || $G.onValidationError.notify( {
-							validationResults: validationResults,
-							row: $G.getDataLength() - 1,
-							cell: i,
-							column: column,
-							item: row
-						} );
+						adds[ id ] = adds[ id ] || {};
+						adds[ id ][ column.id ] = settings.cssClass;
 					}
+
+					this.getData().addItem( row );
+
+					dataView.endUpdate();
+
+					for ( var i = columns.length; --i >= 0; ) {
+						
+						var column = columns[ i ]
+
+						, validator = column.validator;
+
+						if ( "function" === typeof validator ) {
+							
+							var validationResults = validator( row[ column[ "field" ] ], row, column );
+
+							validationResults.valid || this.onValidationError.notify( {
+								validationResults: validationResults,
+								row: this.getDataLength() - 1,
+								cell: i,
+								column: column,
+								item: row
+							} );
+						}
+					}
+
 				}
 
-				$G.setCellCssStyles( settings.add.key, hash );
-				$G.scrollRowIntoView( idx );
-				$G.onAddNewRow.notify( { row: row } );
+				this.scrollRowIntoView( this.getDataLength() );
+				this.onCellCssStylesChanged.notify( { key: settings.key } );
+				this.onAddNewRow.notify( { row: row } );
 			}
 		} );
 	};
@@ -168,9 +152,6 @@ define( function() {
 		var settings = $.extend( {}, defaults, options || {} )
 			
 		, plugin = new Add( $G, settings );
-
-		$G.getData().syncGridCellCssStyles( $G, settings.add.key );
-		$G.getData().syncGridCellCssStyles( $G, settings.invalid.key );
 
 		$G.registerPlugin( plugin );
 
