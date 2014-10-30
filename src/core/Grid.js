5568c174fd8ec10a7c62c53c1d97fe97f4edfd5b
Trazyn <Trazyn@users.noreply.github.com>
2014-10-30 23:28:03 +0800

    Update Grid.js

--- src/core/Grid.js
+++ src/core/Grid.js
@@ -1450,7 +1450,7 @@ if (typeof Slick === "undefined") {
     function appendCellHtml(stringArray, row, cell, colspan, item) {
       var m = columns[cell];
       var cellCss = "slick-cell l" + cell + " r" + Math.min(columns.length - 1, cell + colspan - 1) +
-          (m.cssClass ? " " + m.cssClass : "");
+          (m.cssClass ? " " + m.cssClass : "") +(m.enableCopy ? " slick-copy" : "");
       if (row === activeRow && cell === activeCell) {
         cellCss += (" active");
       }
@@ -1462,15 +1462,16 @@ if (typeof Slick === "undefined") {
         }
       }
 
-      stringArray.push("<div class='" + cellCss + "'>");
+      stringArray.push("<div class='" + cellCss + "'><div class='outer'>");
 
       // if there is a corresponding row (if not, this is the Add New row or this data hasn't been loaded yet)
       if (item) {
         var value = getDataItemValueForColumn(item, m);
         stringArray.push(getFormatter(row, m)(row, cell, value, m, item));
+        m.enableCopy && stringArray.push("<div class='slick-copy-inner'></div>");
       }
 
-      stringArray.push("</div>");
+      stringArray.push("</div></div>");
 
       rowsCache[row].cellRenderQueue.push(cell);
       rowsCache[row].cellColSpans[cell] = colspan;
